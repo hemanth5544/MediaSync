1df24cca596ffb6215157a012a4e0d26ba3ccf62
hemanth5544 <rachapalli.hemanth5544@gmail.com>
2025-04-30 18:32:39 +0530

    doamin changes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "https://media-sync-beta.vercel.app",  
+        origin: "https://mediasync.onrender.com",  
         methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE", "PATCH"]
     }
 });
